a0700200817f90d50d75eb51edf5fa75ea7ef01c
Cristo <cristovdw@outlook.com>
2025-05-22 14:28:20 +0200

    added writing to excelsheet

--- toets-tools/ExportBevindingenLayer.py
+++ toets-tools/ExportBevindingenLayer.py
@@ -1,6 +1,8 @@
 import arcpy
 import os
 import zipfile
+import shutil
+import openpyxl
 #interpreter C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3
 
 #Environment
@@ -36,7 +38,7 @@ def writeOutput(toetspath, inputlayer):
     #Vul X en Y coördinaten in
     arcpy.management.CalculateGeometryAttributes(inputlayer, [["xcoord", "POINT_X"],
                                                               ["ycoord", "POINT_Y"]])
-        
+    
     #Save feature (alles)
     arcpy.AddMessage(f"totaal-shapefile opslaan")
     shp_outputpath = rf"{toetspath}\toets_totaal\bevindingen-totaal.shp"
@@ -91,5 +93,59 @@ def writeOutput(toetspath, inputlayer):
                            + f"{omschrijving}\n"
                            + f"{bijlagetxt}\n"
                            + "--------------------------------\n")
-
+                
+    #beoordelingsrapport kopiëren
+    arcpy.AddMessage(f"Beoordelingsrapport kopiëren")
+    standard_rapport = rf"G:\civ\IGA_ATG\Producten\DTB\Toetstooling\beoordelingsrapport_template\beoordelingsrapport_dtb.xlsm"
+    rapport_loc = rf"{toetspath}\job{ivrinummer}_beoordelingsrapport_{toetsnr}.xlsm"
+    shutil.copy2(standard_rapport, rapport_loc)
+    excel = openpyxl.load_workbook(rapport_loc, keep_vba=True)
+    excelsheet = excel["Beoordelingsrapport"]
+    
+    #standaardcellen, moet overeenkomen met de 'invulvelden.txt' van het dropdownmenu/domein
+    vraag_cel_map = {
+        "Hier wordt niet voldaan aan de eisen gesteld aan de bestandsopbouw": "C91",
+        "Hier wordt niet voldaan aan de eisen gesteld aan de attribuutwaarden": "C98",
+        "Hier wordt niet voldaan aan de eisen gesteld aan de inwinning van objecten": "C105",
+        "Hier sluit het nieuwe DTB niet goed aan op het oude DTB": "C112",
+        "Hier wordt niet voldaan aan de eisen gesteld aan de volledigheid": "C119",
+        "Hier wordt niet voldaan aan de eisen gesteld aan de meetpunten": "C126"
+    }
+    
+    #schrijf een standaardtekst in de cel voor elke shape
+    arcpy.AddMessage(f"Beoordelingsrapport vullen")
+    with arcpy.da.SearchCursor(inputlayer, [kolom1, kolom2, kolom3, kolom4, kolom5, kolom6]) as cursor:
+            for row in cursor:
+                
+                #lees attributentabel
+                volgnummer = row[0] if row[0] is not None else ""
+                fout = row[1] if row[1] is not None else ""
+                omschrijving = row[2] if row[2] is not None else ""
+                bijlage = row[3] if row[3] is not None else ""
+                if bijlage == "":
+                    bijlagetxt = ""
+                else:
+                    bijlagetxt = f"Zie bijlage {bijlage}."
+                xcoord = row[4] if row[4] is not None else ""
+                ycoord = row[5] if row[5] is not None else ""
+                
+                #maak er een volledig stuk standaardtekst van
+                fulltext = (
+                    f"Zie volgnummer {volgnummer} in het meegestuurde shapefile bestand, "
+                    f"ter plaatse van x={xcoord}, y={ycoord}.\n"
+                    f"{fout}.\n"
+                    f"{omschrijving}\n"
+                    f"{bijlagetxt}\n"
+                    "--------------------------------\n"
+                )
+                
+                #voeg het toe in de besbetreffende cel
+                targetcell = vraag_cel_map.get(fout)
+                cellvalue = excelsheet[targetcell].value
+                if cellvalue is None:
+                    cellvalue = ""
+                newcellvalue = cellvalue + fulltext
+                excelsheet[targetcell] = newcellvalue
+    excel.save(rapport_loc)
+    
 checkIVRIpath(ivrinummer)
